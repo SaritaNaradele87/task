10140c5a3a575de03dd83072ebd7f12f276f11a6
SaritaNaradele87 <143347028+SaritaNaradele87@users.noreply.github.com>
2024-12-07 16:29:15 +0530

    Update CourseRepository.java

--- Course_Management_System/src/main/java/com/management/repository/CourseRepository.java
+++ Course_Management_System/src/main/java/com/management/repository/CourseRepository.java
@@ -1,5 +1,11 @@
 package com.management.repository;
 
-public class CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.management.entity.Course;
+
+@Repository
+public interface CourseRepository  extends JpaRepository<Course, Integer>{
+ 
 }
