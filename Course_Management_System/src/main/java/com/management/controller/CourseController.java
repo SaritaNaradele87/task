4c54c3c3af3955f457985bd4a56ea183fed8e726
SaritaNaradele87 <143347028+SaritaNaradele87@users.noreply.github.com>
2024-12-07 16:26:01 +0530

    Update CourseController.java

--- Course_Management_System/src/main/java/com/management/controller/CourseController.java
+++ Course_Management_System/src/main/java/com/management/controller/CourseController.java
@@ -1,19 +1,46 @@
 package com.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import com.management.courseService.Service;
+import com.management.entity.Course;
+import com.management.courseService.CourseService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("courses")
 public class CourseController {
-	
-	@Autowired
-	Service service;
-	@
-	public String AddCourse() {
-		return service.Addcourse();
-		
-	}
 
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<Course> addCourse(@RequestBody Course course) {
+        Course savedCourse = courseService.addCourse(course);
+        return ResponseEntity.ok(savedCourse);
+    }
+
+   
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> updateCourseById(
+            @PathVariable Integer id,
+            @RequestBody Course course) {
+        Course updatedCourse = courseService.updateCourseById(id, course);
+        return ResponseEntity.ok(updatedCourse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courses = courseService.getAllCourses();
+        return ResponseEntity.ok(courses);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCourseById(@PathVariable Integer id) {
+        String result = courseService.deleteCourseById(id);
+        return ResponseEntity.ok(result);
+    }
 }
